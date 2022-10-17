423fde5d186e72a21d4736da5aaa9f339b63b09a
Adrian Debowski <adrian.debowski@outlook.com>
2022-10-17 12:12:48 +0200

    Created class for Item

---
+++ src/Item.java
@@ -0,0 +1,25 @@
+
+//Class representing Item to be carried
+
+class Item {
+    private String name;
+    private int weight;
+
+    //Set name of item
+    void setName(String name) {
+        this.name = name;
+    }
+
+    // Set weight of item
+    void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    String getName() {
+        return name;
+    }
+    
+    int getWeight() {
+        return weight;
+    }
+}
